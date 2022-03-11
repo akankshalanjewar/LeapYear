880b841b9687bef5dd43cd475bd0034f91e59ea2
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-11 10:11:09 +0530

    added[akanksha]

---
+++ LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int year;
+
+            Console.WriteLine("Enter the year");
+
+            Random random = new Random();
+
+            year = int.Parse(Console.ReadLine());
+
+
+
+            if ((year % 100 == 0) && (year % 400 == 0) || (year % 4 == 0))
+            {
+
+                Console.WriteLine("is a leap year");
+            }
+
+            else
+            {
+
+                Console.WriteLine("is not a leap year");
+            }
+
+        }
+    }
+}
